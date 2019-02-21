50eb63ebcf462af6b9b008b8d4965765174ec617
Renkosky <philsky28@gmail.com>
2019-02-21 13:51:33 +0800

    update readme,delete useless import

---
+++ readme.md
@@ -0,0 +1,28 @@
+# Cyberspace
+
+![npm](https://img.shields.io/badge/npm-v6.4.1-blue.svg)
+![node](https://img.shields.io/badge/node-%3E%3D11.0.0-brightgreen.svg)
+![react](https://img.shields.io/badge/react-%5E16.4.0-blue.svg)
+![antd-mobile](https://img.shields.io/badge/%20antd--mobile-%5E2.1.11-blue.svg)
+
+## 介绍
+
+一个整合了新闻咨询以及论坛功能的 H5 APP
+
+## 技术栈
+
+React 全家桶，axios，antd-mobile，less
+
+## 功能列表
+
+- [ ] 咨询页面
+- [x] 用户登录/注册页
+- [x] 个人设置页面
+- [ ] 个人信息详情页
+- [ ] 管理个人帖页
+- [x] 论坛首页
+- [x] 发帖弹窗
+- [x] 登出按钮
+- [ ] 帖子详情页
+- [ ] 咨询贴详情页
+- [ ] 管理员账户管理页
--- src/pages/Route.js
+++ src/pages/Route.js
@@ -2,10 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Register from "./Register";
-import News from "./News";
-import Park from "./Park";
-import Me from "./Me";
-import ListViews from "./ListView";
+
 // import re from 'components/Login'
 class AppRoute extends Component {
   render() {
@@ -13,7 +10,6 @@ class AppRoute extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/signup" component={Register} />
-          <Route exact path="/test" component={ListViews} />
           <Route exact path="/" component={NavBar} />
         </Switch>
       </BrowserRouter>
